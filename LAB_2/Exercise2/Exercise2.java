807b3cf3cb0e107c7054e9ead15505b4cd5876c9
Sayan Banerjee <banerjeesayan09082002@gmail.com>
2023-09-05 18:29:33 +0530

    Update Exercise2.java

--- LAB_2/Exercise2/Exercise2.java
+++ LAB_2/Exercise2/Exercise2.java
@@ -1,1 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Exercise2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your First Name: ");
+        String name = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("Yourname is: " + name);
+    }
+}
