01bd5a90d96b7894e4babda910c44a6a9a29388b
Sayan Banerjee <banerjeesayan09082002@gmail.com>
2023-09-05 18:23:16 +0530

    Update Exercise1.java

--- LAB_2/Exercise1/Exercise1.java
+++ LAB_2/Exercise1/Exercise1.java
@@ -1,1 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Exercise1 {
+    static int adder(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++)
+            sum = sum + numbers[i];
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int[] numbers = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter integer number " + (i+1) + ":  ");
+            String input = reader.readLine();
+
+            try {
+                numbers[i] = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Only integers are accepted as input");
+                i--;
+            }
+        }
+
+        System.out.println("The sum of the integers are = " + adder(numbers));
+    }
+}
